339f554aa801a2f19594ab0a9064ffdd7682ff90
Chase Steenbock <chase@pop-os.localdomain>
2020-06-17 22:57:36 -0500

    trying to figure out next js

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxBracketSameLine: true,
+  quoteProps: 'consistent',
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none'
+};
