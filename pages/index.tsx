501e421fc84380af7ed3f39f428465d30f736ab5
Chase Steenbock <chase.steenbock@gmail.com>
2020-06-18 00:49:05 -0500

    got storyblok api working with environment variable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import App from './_app';
-
 import Head from 'next/head';
 import StoryblokClient from 'storyblok-js-client';
 import useSWR from 'swr';
+import ReactMarkdown from 'react-markdown';
 
-export const api_key = process.env.storyblok_key;
+export const api_key = process.env.NEXT_PUBLIC_STORYBLOK;
 
 export const Storyblok = new StoryblokClient({
   accessToken: api_key,
@@ -18,6 +17,8 @@ const Home = () => {
   const { data, error } = useSWR('home', story =>
     Storyblok.get(`cdn/stories/${story}`)
   );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
 
   return (
     <div className="container">
@@ -28,7 +29,7 @@ const Home = () => {
 
       <main>
         <h1 className="title">Welcome to my blog!</h1>
-        {data ? data.data.content.body : null}
+        <ReactMarkdown source={data ? data.data.story.content.body : null} />
       </main>
     </div>
   );
